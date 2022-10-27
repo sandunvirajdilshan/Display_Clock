5c7c816a8a5db43ec3e00703271cb8c5991021b4
Sandun Viraj Dilshan <110237864+sandunvirajdilshan@users.noreply.github.com>
2022-10-27 18:13:19 +0530

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,70 @@
+
+window.addEventListener("DOMContentLoaded", showTime());
+
+function appmenu(){
+  
+}
+
+function showTime() {
+  let date = new Date();
+
+  let h = date.getHours();
+  let m = date.getMinutes();
+  let s = date.getSeconds();
+  let session = "AM";
+
+  if (h == 0) {
+    h = 12;
+  }
+  if (h > 12) {
+    h = h - 12;
+    session = "PM";
+  }
+
+  h = h < 10 ? (h = "0" + h) : h;
+  m = m < 10 ? (m = "0" + m) : m;
+  s = s < 10 ? (s = "0" + s) : s;
+
+  const time = `${h}:${m}<small>|${session}</small>`;
+
+  document.getElementById("displayClock").innerHTML = time;
+  setTimeout(showTime, 1000);
+
+  let bg;
+  const userName = "Sandun.";
+  const user = document.getElementById("User");
+  const title = document.getElementById("title");
+
+  bg = 'url(/assets/background.jpg)';
+
+  if (h >= 5 && h <= 8 && session === "AM") {
+    user.innerHTML = `Good Morning.., ${userName}`;
+    title.innerHTML = `Good Morning`;
+  } else if (h > 8 && h < 12 && session === "AM") {
+    user.innerHTML = `Good Morning.., ${userName}`;
+    title.innerHTML = `Good Morning`;
+  } else if (h >= 1 && h <= 3 && session === "PM") {
+    user.innerHTML = `Good Day.., ${userName}`;
+    title.innerHTML = `Good Day`;
+  } else if (h <= 7 && session === "PM") {
+    user.innerHTML = `Good Evening.., ${userName}`;
+    title.innerHTML = `Good Evening`;
+  } else {
+    user.innerHTML = `Good Night.., ${userName}`;
+    title.innerHTML = `Good Night`;
+  }
+
+  const body = document.querySelector("body");
+  body.style.background = `${bg} center/cover`;
+}
+
+document
+  .querySelector(".focus-container input")
+  .addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      const focus = document.querySelector(".focus-container input");
+      document.querySelector(
+        ".focus-container"
+      ).innerHTML = `<h6>TODAY:</h6><h1>${focus.value}</h1>`;
+    }
+  });
