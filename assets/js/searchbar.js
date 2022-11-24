24f51f7f9fef0048f5600e991ba702c4273aa652
Sandun Viraj Dilshan <110237864+sandunvirajdilshan@users.noreply.github.com>
2022-11-24 11:22:17 +0530

    Add files via upload

---
+++ assets/js/searchbar.js
@@ -0,0 +1,10 @@
+const f = document.getElementById('form');
+const q = document.getElementById('query');
+const google = 'https://www.google.com/search?q=';
+function submitted(event){
+  const url = google + q.value;
+  const win = window.open(url, '_blank');
+  win.focus();
+}
+
+f.addEventListener('submit', submitted);
